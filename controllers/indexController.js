81716b7bed005a04cedf5b6a46ae00519299165e
Hoshin <martin.bahier@gmail.com>
2014-10-18 22:15:03 +0200

    App now sends the correct JSON / GUI updated to match definitive JSON

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -23,8 +23,23 @@ IndexController.prototype.receiveSound = function (fields, files) {
 
 IndexController.prototype.sentenceGrading = function (req) {
     var fleschKincaid = new SentenceGradeLevel(req.body.sentence);
+
+//    {
+//        "buzzwords": [
+//        "foo",
+//        "bar"
+//    ],
+//        "grade": 12,
+//        "flesch-kincaid": 11.456,
+//        "recognized_text": "blabla blabla"
+//    }
+    var buzz = buzzDetector.buzzPerTotalwords(req.body.sentence);
     var gradingDetails = {
-        buzz: buzzDetector.buzzPerTotalwords(req.body.sentence), fleschKincaid:fleschKincaid.grade()
+        buzzwords:buzz.suspects,
+        ratio:buzz.ratio,
+        grade:0,
+        'flesch-kincaid':fleschKincaid.grade(),
+        recognized_text:req.body.sentence
     };
     return gradingDetails;
 };
