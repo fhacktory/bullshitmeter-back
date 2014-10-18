75252b4a01738d4164051752b7c9c35565d49965
Hoshin <martin.bahier@gmail.com>
2014-10-18 13:03:14 +0200

    Cleanup : * Removed now useless requires in main controller. * Cleaned up controller constructor

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,7 @@
-var buzzwords = require('buzzwords');
-var _ = require('lodash');
 var SentenceGradeLevel = require('../app_modules/sentenceGradeLevel');
 var BuzzDetector = require('../app_modules/buzzDetector');
 
 var IndexController = function () {
-    var detector = new BuzzDetector();
 
 };
 
