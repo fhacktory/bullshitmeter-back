9c44abede9393afd3532cd865680fb0b3cdeac88
Hoshin <martin.bahier@gmail.com>
2014-10-18 22:29:17 +0200

    more cleanup + added score calculation and GUI display

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,7 @@
 var SentenceGradeLevel = require('../app_modules/sentenceGradeLevel');
 var buzzDetector = new(require('../app_modules/buzzDetector'))();
 
-var IndexController = function () {
-
-};
-
-IndexController.prototype.scorePhrase = function (sentence) {
-    return buzzDetector.buzzPerTotalwords(sentence);
-};
+var IndexController = function () {};
 
 IndexController.prototype.receiveSound = function (fields, files) {
     var sound = files['soundFile'];
@@ -25,11 +19,12 @@ IndexController.prototype.sentenceGrading = function (req) {
     var fleschKincaid = new SentenceGradeLevel(req.body.sentence);
 
     var buzz = buzzDetector.buzzPerTotalwords(req.body.sentence);
+    var fkGrade = fleschKincaid.grade()
     var gradingDetails = {
         buzzwords:buzz.suspects,
         ratio:buzz.ratio,
-        grade:0,
-        'flesch-kincaid':fleschKincaid.grade(),
+        grade:Math.min(buzz.ratio*fkGrade, 20),
+        'flesch-kincaid':fkGrade,
         recognized_text:req.body.sentence
     };
     return gradingDetails;
