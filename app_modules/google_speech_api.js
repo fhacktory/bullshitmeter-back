4c3eba1171ddbbba399b1e03bb237899d70531d5
Vincent Daubry <vdaubry@gmail.com>
2014-10-18 21:40:09 +0200

    google speech api template

---
+++ app_modules/google_speech_api.js
@@ -0,0 +1,9 @@
+var GoogleSpeechApi = function(){};
+var exec = require('child_process').exec;
+
+SentenceGradeLevel.prototype.readSound = function(){
+	//exec("pwd", function (error, stdout, stderr) {});
+  return "text";
+};
+
+module.exports = GoogleSpeechApi;
