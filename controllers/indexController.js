f72feafa95a908134bb34b04898768ed455dd740
Vincent Daubry <vdaubry@gmail.com>
2014-10-18 19:21:00 +0200

    merge

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -9,22 +9,16 @@ IndexController.prototype.scorePhrase = function (sentence) {
     return buzzDetector.buzzPerTotalwords(sentence);
 };
 
-<<<<<<< HEAD
 IndexController.prototype.receiveSound = function (sound) {
     return JSON.stringify({"status": "ok"});
 };
 
-IndexController.prototype.sentenceGrading = function(){
-  var sentenceGradeLevel = new SentenceGradeLevel();
-
-=======
 IndexController.prototype.sentenceGrading = function (req) {
     var fleschKincaid = new SentenceGradeLevel(req.body.sentence);
     var gradingDetails = {
         buzz: buzzDetector.buzzPerTotalwords(req.body.sentence), fleschKincaid:fleschKincaid.grade()
     };
     return gradingDetails;
->>>>>>> fdd797a51c9fd6d8963c3770931504609f547bb2
 };
 
 module.exports = IndexController;
