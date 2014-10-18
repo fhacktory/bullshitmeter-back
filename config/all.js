8a091528ea12fb8715b59735f5ac4ac383d00f69
Hoshin <martin.bahier@gmail.com>
2014-10-18 17:56:27 +0200

    Fixed a config issue (development.js not used, not needed but still loaded ... this is not the case anymore)

--- config/all.js
+++ config/all.js
@@ -5,6 +5,4 @@ var _ = require('lodash');
 /**
  * Load environment configuration
  */
-module.exports = _.merge(
-    require('./env/all.js'),
-    require('./env/' + process.env.NODE_ENV + '.js') || {});
+module.exports = require('./env/all.js');
