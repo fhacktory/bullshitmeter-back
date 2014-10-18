13b9b7b453936f2bb3fd01b73db19662d713da12
Hoshin <martin.bahier@gmail.com>
2014-10-18 12:43:15 +0200

    added missing router ...

---
+++ routes/indexRouter.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var indexController = new (require('../controllers/indexController.js'))();
+
+var session = require('express-session');
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'development'
+}
+
+var config = require('../config/all.js');
+
+
+var app = express();
+
+app.use(session({ secret: 'Some Secret !!!', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+router.get('/', function(req, res, next){
+    res.render('scoreMeForm');
+});
+
+router.get('/score-me', function(req, res, next){
+    res.json({score:indexController.scorePhrase(req.body.phrase)});
+});
+
+module.exports = router;
