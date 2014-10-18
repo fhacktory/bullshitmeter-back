4a06cac4205065c97cc3d6fa94c9f865419ac544
Hoshin <martin.bahier@gmail.com>
2014-10-18 22:50:50 +0200

    buzzDetector now handles composed buzzwords

--- app_modules/buzzDetector.js
+++ app_modules/buzzDetector.js
@@ -4,26 +4,34 @@ var buzzword = require('buzzwords');
 var cleanupUtils = new (require('../utils/cleanupUtils.js'))();
 
 var BuzzDetector = function () {
-    _.forEach(additionalWords, function(additionalWord){
+    _.forEach(additionalWords, function (additionalWord) {
         buzzword.add(additionalWord);
     });
 };
 
 BuzzDetector.prototype.amIBuzzing = function (sentence) {
     //diacritics / punctuation removal before ?
+    var buzzPressionsMatches = [];
+    _.forEach(additionalWords, function (additionalWord) {
+        if (additionalWord.indexOf(' ') > -1) {
+            if (sentence.indexOf(additionalWord) > -1) {
+                buzzPressionsMatches.push(additionalWord);
+            }
+        }
+    });
     var cleanedWords = cleanupUtils.removeStopWords(sentence);
     var words = cleanedWords.split(' ');
     var buzzWords = _.filter(words, function (word) {
         return(buzzword.is(word));
     });
-    return buzzWords;
+    return buzzWords.concat(buzzPressionsMatches);
 };
 
-BuzzDetector.prototype.buzzPerTotalwords = function(sentence){
+BuzzDetector.prototype.buzzPerTotalwords = function (sentence) {
     var cleanedWords = cleanupUtils.removeStopWords(sentence);
     var totalBuzz = this.amIBuzzing(sentence);
     var computedRatio = (totalBuzz.length / cleanedWords.split(' ').length);
-    var result = {ratio:computedRatio, suspects:totalBuzz};
+    var result = {ratio: computedRatio, suspects: totalBuzz};
     return result;
 };
 
